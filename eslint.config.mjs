9c3c875bf3232e5fa51810694ef709cd172e9478
George Borisov <git@gir.me.uk>
2024-11-01 23:12:22 +0000

    upgrade packages and migrate eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default tseslint.config({
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintPluginPrettierRecommended,
+  ],
+  files: ['src/**/*.ts'],
+  rules: {
+    'no-console': ['warn'],
+  },
+});
